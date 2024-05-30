9b7c1c4007943494c4560a01c8be7d07b2b13d4c
xegby <xegby90@gmail.com>
2024-05-31 02:08:57 +0300

    refactoring safer media moves download all media types remove filename suffixes

--- common.py
+++ common.py
@@ -4,36 +4,37 @@ import filecmp
 
 # moves file with checks
 def move_file(srcpath,dstpath,allow_suffix=True):
-    # remove any '_copyNN' from dest
+    # remove any '_copyNN' suffix from dest
     if allow_suffix:
         dstpath=re.sub(r'_copy[0-9]*', '', dstpath)
     # the same path
     if srcpath==dstpath:
-        return True
+        return dstpath
     # create directory
     os.makedirs(os.path.dirname(dstpath),exist_ok=True)
     # move file if it does not exist
     if not os.path.exists(dstpath):
         os.link(srcpath,dstpath)
         os.unlink(srcpath)
-        return True
+        return dstpath
     # the same file, delete source
     if filecmp.cmp(srcpath,dstpath):
-        os.remove(srcpath)
-        return True
+        os.unlink(dstpath)
+        return dstpath
     # check if directory
     dststat=os.stat(dstpath)
     if stat.S_ISDIR(dststat.st_mode):
         if not allow_suffix:
-            return False
+            return None
     # files different, create copy
     if allow_suffix:
         for i in range(1,32):
-            if move_file(srcpath,"{0}_copy{2}{1}".format(*(os.path.splitext(dstpath)),i),False):
-                return True
+            newdest=move_file(srcpath,"{0}_copy{2}{1}".format(*(os.path.splitext(dstpath)),i),False)
+            if newdest:
+                return newdest
         print('can not move '+srcpath+' to '+dstpath)
     # la problema
-    return False
+    return None
 
 # compares two dictionaries ignoring sprcific keys
 def compare_dict(a, b, ignore_keys={}):
--- photo_albums.py
+++ photo_albums.py
@@ -39,29 +39,36 @@ def LoadAlbums(dest):
             items={}
             data['path']=str(path.absolute().parent)
             albums[data['id']]=data
+    return albums
+
+# load ignore list
+def LoadIgnore(dest):
     ignore=[]
     for path in dest.rglob('ignore.*'):
         with path.open() as file:
-            ignore.append(file.read().splitlines())
-    return(albums,ignore)
+            ignore+=file.read().splitlines()
+    return ignore
 
 # dowload all albums
 def DowloadAlbums(req,orig,trash,dest,old,ignore):
     nextpage='0'
     new={}
     while nextpage is not None:
+        # get album data
         if nextpage=='0':
-            (resp, content) = req.request("https://photoslibrary.googleapis.com/v1/albums", method="GET")
+            (resp, content) = req.request("https://photoslibrary.googleapis.com/v1/albums?pageSize=50", method="GET")
         else:
-            (resp, content) = req.request("https://photoslibrary.googleapis.com/v1/albums?pageToken="+nextpage,method="GET")
+            (resp, content) = req.request("https://photoslibrary.googleapis.com/v1/albums?pageSize=50&pageToken="+nextpage,method="GET")
         if resp.status is not 200:
             print('failed to load albums',resp.status,resp.reason)
             return
         data = json.loads(content)
+        # remember nextpage
         if 'nextPageToken' in data:
             nextpage=data['nextPageToken']
         else:
             nextpage=None
+        # store albums' data by their last download time
         for album in data['albums']:
             if album['id'] in old and 'downloadTime' in old[album['id']]:
                 album['path']=old[album['id']]['path']
@@ -69,9 +76,10 @@ def DowloadAlbums(req,orig,trash,dest,old,ignore):
             else:
                 album['path']=str(dest/album['title'])
                 new[0] = new.get(0, [])+[album]
-#    del new[0]
+    # download albums starting from the oldest downloaded
     for k,albums in sorted(new.items()):
         for album in albums:
+            # download only allowed albums
             if album['title'] not in ignore and album['id'] not in ignore:
                 if album['id'] in old:
                     DowloadAlbum(req,orig,trash,album,old[album['id']])
@@ -83,107 +91,156 @@ def DowloadAlbum(req,orig,trash,album,old_album):
     album['mediaItems']={}
     dest=Path(album['path'])
     dest.mkdir(parents=True,exist_ok=True)
+    req_body=json.dumps({"albumId": album['id'],"pageSize": 100});
+    req_headers={'content-type':'application/json'}
     nextpage='0'
+    res=True
     while nextpage is not None:
+        # request album data
         if nextpage=='0':
-            (resp, content) = req.request("https://photoslibrary.googleapis.com/v1/mediaItems:search",method="POST",
-                                           body=json.dumps({"albumId": album['id']}),
-                                           headers={'content-type':'application/json'})
+            (resp, content) = req.request("https://photoslibrary.googleapis.com/v1/mediaItems:search",
+                                           method="POST",body=req_body,headers=req_headers)
         else:
-            (resp, content) = req.request("https://photoslibrary.googleapis.com/v1/mediaItems:search?pageToken="+nextpage,method="POST",
-                                           body=json.dumps({"albumId": album['id']}),
-                                           headers={'content-type':'application/json'})
+            (resp, content) = req.request("https://photoslibrary.googleapis.com/v1/mediaItems:search?pageToken="+nextpage,
+                                           method="POST",body=req_body,headers=req_headers)
         if resp.status is not 200:
             print('failed to load album',album['title'],resp.status,resp.reason)
             return False
         items = json.loads(content)
+        # prepare next token
         if 'nextPageToken' in items:
             nextpage=items['nextPageToken']
         else:
             nextpage=None
+        # download album media
         for media in items['mediaItems']:
             if old_album is not None and media['id'] in old_album['mediaItems']:
-                res=DowloadMedia(req,orig,trash,album,media,old_album['mediaItems'][media['id']])
+                res=CheckMedia(req,orig,trash,album,media,old_album['mediaItems'][media['id']])
             else:
-                res=DowloadMedia(req,orig,trash,album,media,None)
+                res=DowloadMedia(req,orig,trash,album,media)
             if not res:
-                return False
+                break
+        # keep old album data
+        if not res:
+            if old_album is not None:
+                for media in old_album['mediaItems'].items():
+                    if media['id'] not in album['mediaItems']:
+                        album['mediaItems'][media['id']]=media
+            break
     # set album download time and store its metadata
     album['downloadTime']=datetime.now(timezone.utc).timestamp()*1000
     with (Path(album['path'])/'album.json').open("w") as file:
         json.dump(album,file,indent=2)
+    # stop here if something went wrong
+    if not res:
+        return False
     # build set of current album items filenames and remove any excess files from directory
     filenames={'album.json'}
     for id,media in album['mediaItems'].items():
         filenames.add(media['filename'])
     for file in Path(album['path']).glob('*'):
         if file.name not in filenames:
-            moved=False
+            moved=None
             if trash is not None:
                 moved=common.move_file(str(file),str(trash/album['title']/file.name))
             if moved:
                 print(album['title'],file.name,'excessive file moved to trash directory')
             else:
                 dest.unlink()
-                print(album['title'],file.name,'excessive file deleted')
+                print(album['title'],file.name,'excessive file removed')
+    # try to rename media files to original filenames
+    changed=False
+    for id,media in album['mediaItems'].items():
+        filename=Path(album['path'],media['filename'])
+        if filename.exists():
+            filename=common.move_file(str(filename),str(filename))
+            if filename:
+                filename=Path(filename)
+                if media['filename']!=filename.name:
+                    print(album['title'],filename.name,'renamed from',media['filename'])
+                    media['filename']=filename.name
+                    changed=True
+    # and store it again
+    if changed:
+        with (Path(album['path'])/'album.json').open("w") as file:
+            json.dump(album,file,indent=2)
+    # ok
+    print(album['title'],"is up to date now")
     return True
 
-# dowload media
-def DowloadMedia(req,orig,trash,album,media,old):
-    if 'mimeType' in media and media['mimeType'].startswith('image'):
-        return DowloadPhoto(req,orig,trash,album,media,old)
-    return True
-
-# dowload photo
-def DowloadPhoto(req,orig,trash,album,photo,old,idx=0):
-    if idx>32:
-        return False
-    dest=Path(album['path'])/photo['filename']
-    if idx!=0:
-        dest=Path("{1}_copy{2}{3}".format(dest.stem,idx,dest.suffix))
-    # file already exists
-    if dest.exists():
-        if old is not None and common.compare_dict(photo,old,{'baseUrl'}):
-            # if nothing is changed since last download, just remember item
-            photo['filename']=dest.name
-            album['mediaItems'][photo['id']]=photo
-            print(album['title'],photo['filename'],'already exists')
-            return True
+# check existing media
+def CheckMedia(req,orig,trash,album,media,old):
+    # fake
+    if old is None or 'filename' not in old:
+        return DowloadMedia(req,orig,trash,album,media)
+    # check is exists
+    oldpath=Path(album['path'],old['filename'])
+    if not oldpath.exists():
+        return DowloadMedia(req,orig,trash,album,media)
+    # something changed
+    if not common.compare_dict(media,old,{'baseUrl','filename'}):
+        # move outdated file to trash directory
+        moved=None
+        if trash is not None:
+            moved=common.move_file(str(oldpath),str(trash/album['title']/old['filename']))
+        if moved:
+            print(album['title'],oldpath.name,'outdated file moved to trash directory')
         else:
-            # there is different media with the same filename in album, try with different number
-            for (k,dup) in album['mediaItems'].items():
-                if dup['filename']==dest.name:
-                    return DowloadPhoto(req,orig,trash,album,photo,old,idx+1)
-            # move existing file to trash directory
-            moved=False
-            if trash is not None:
-                moved=common.move_file(str(dest),str(trash/album['title']/photo['filename']))
-            if moved:
-                print(album['title'],dest.name,'excessive file moved to trash directory')
-            else:
-                dest.unlink()
-                print(album['title'],dest.name,'excessive file deleted')
-    # download photo
-    for t in [1,2,5,15,30,60,sys.maxsize]:
-        (resp, content) = req.request(photo['baseUrl']+'=d', method="GET")
+            dest.unlink()
+            print(album['title'],oldpath.name,'outdated file deleted')
+        # download new media
+        return DowloadMedia(req,orig,trash,album,media)
+    # set old filename
+    media['filename']=old['filename']
+    # store media data to album
+    album['mediaItems'][media['id']]=media
+    # ok
+    print(album['title'],media['filename'],'already exists')
+    return True
+
+# dowload new media
+def DowloadMedia(req,orig,trash,album,media):
+    # can we store it?
+    if 'filename' not in media:
+        return True
+    # format temp destination
+    dest=Path(album['path'],media['filename'])
+    tmp=dest.parent/(dest.name+'.tmp')
+    # file already exists
+    if tmp.exists():
+        tmp.unlink()
+        print(album['title'],tmp.name,'removed old temp file')
+    # download media
+    for t in [1,2,5,15,30,60,0]:
+        (resp, content) = req.request(media['baseUrl']+'=d', method="GET")
         if resp.status is 200:
             break
-        if t is sys.maxsize:
-            print(album['title'],dest.name,'download failed, max attempts exceeded','[{status} {reason}]'.format(status=resp.status,reason=resp.reason))
+        if t<=0:
+            print(album['title'],tmp.name,'download failed, max attempts exceeded','[{status} {reason}]'.format(status=resp.status,reason=resp.reason))
             return False
-        print(album['title'],dest.name,'download failed, try again in',t,'seconds','[{status} {reason}]'.format(status=resp.status,reason=resp.reason))
+        print(album['title'],tmp.name,'download failed, try again in',t,'seconds','[{status} {reason}]'.format(status=resp.status,reason=resp.reason))
         time.sleep(t)
-    # save photo
-    with dest.open('wb') as f:
+    # save media
+    with tmp.open('wb') as f:
         f.write(content)
-    # save metadata
-    photo['filename']=dest.name
-    album['mediaItems'][photo['id']]=photo
-    print(album['title'],photo['filename'],'downloaded')
+    if not tmp.exists():
+        print(album['title'],tmp.name,'failed to save file')
+        return False
+    # rename
+    filename=common.move_file(str(tmp),str(dest))
+    if filename:
+        dest=Path(filename)
+    else:
+        print(album['title'],dest.name,'failed to save file')
+        return False
+    # save media data to album
+    media['filename']=dest.name
+    album['mediaItems'][media['id']]=media
+    print(album['title'],media['filename'],'downloaded')
     return True
-
 
 # main flow
 req=Authorize(Path(args.key_file),Path(args.creds_file))
-old_albums,ignore_albums=LoadAlbums(Path(args.destination))
+old_albums=LoadAlbums(Path(args.destination))
+ignore_albums=LoadIgnore(Path(args.destination))
 DowloadAlbums(req,Path(args.originals),Path(args.trashbin),Path(args.destination),old_albums,ignore_albums)
